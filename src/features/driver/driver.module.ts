eee2eed84b9de763eb4509986218315ae82a1ce4
gabrielaragao01 <biel.aragao10@gmail.com>
2025-03-26 20:43:10 -0300

    fix: Update driver module to use correct driver schema

--- src/features/driver/driver.module.ts
+++ src/features/driver/driver.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DriversController } from './controllers/driver.controller';
 import { DriversService } from './services/driver.service';
-import { DriverEntity,  } from './data/entities/driver.entity';
+import { DriverEntity, DriverSchema } from './data/entities/driver.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: DriverEntity.name, schema: DriverEntity },
+      { name: DriverEntity.name, schema: DriverSchema },
     ]),
   ],
   controllers: [DriversController],
