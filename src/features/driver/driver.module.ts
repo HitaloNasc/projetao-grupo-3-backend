cc9f68449bdd048db76cdb2bf77062c2ef4441e0
gabrielaragao01 <biel.aragao10@gmail.com>
2025-03-31 15:58:43 -0300

    fix: driver module to import user module

--- src/features/driver/driver.module.ts
+++ src/features/driver/driver.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DriversController } from './controllers/driver.controller';
 import { DriversService } from './services/driver.service';
 import { DriverEntity, DriverSchema } from './data/entities/driver.entity';
+import { UserModule } from '../users/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: DriverEntity.name, schema: DriverSchema },
     ]),
+    UserModule,
   ],
   controllers: [DriversController],
   providers: [DriversService],
